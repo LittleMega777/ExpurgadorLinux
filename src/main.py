c44b6ee306be092d293cfe2d3bfe9ba6b6f81460
LittleMega777 <pierrecouto02@gmail.com>
2025-05-20 13:27:53 -0300

    feat(main.py) -- add conersor de epoch time

--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
 import os
+from datetime import datetime
 
-def listdir():
-    lista_de_diretorios = os.listdir()
-    print(lista_de_diretorios)
-    return lista_de_diretorios
 
-def retorna_diretorios_e_arquivos(path):
+# def listdir():
+#     lista_de_diretorios = os.listdir()
+#     print(lista_de_diretorios)
+#     return lista_de_diretorios
 
-    arquivos = []
+def retorna_path_de_arquivos(path):
+
+    arquivos_paths = []
 
     for root, dirs, files in os.walk(path):
         for nome_de_arquivo in files:
             caminho_completo = os.path.join(root, nome_de_arquivo)
-            arquivos.append(caminho_completo)
+
+            arquivos_paths.append(caminho_completo)
             print(f"Arquivo: {caminho_completo}")
 
-    return arquivos
+    return arquivos_paths
 
-def verifica_stime():
+def calcula_stime(path):
+    for file in path:        
+        print(f"file: {file} stime: {os.stat(file).st_atime} datetime {converte_timestamp_para_datetime(os.stat(file).st_atime)}")
     ...
+
+def converte_timestamp_para_datetime(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+    
             
--- test/test_expurgo.py
+++ test/test_expurgo.py
@@ -1,12 +1,22 @@
-from src.main import listdir, retorna_diretorios_e_arquivos
+from src.main import listdir, retorna_path_de_arquivos, calcula_stime
 
 
-def test_listdir():
-    list_dire = listdir()
+path_raiz = "."
 
-    assert list_dire
-    assert isinstance(list_dire, list)
+# def test_listdir():
+#     list_dire = listdir()
+
+#     assert list_dire
+#     assert isinstance(list_dire, list)
 
 def test_retorna_diretorios_e_arquivos():
-    retorna_diretorios_e_arquivos(".")
+    lista_de_arquivos = retorna_path_de_arquivos(path_raiz)
+
+    assert isinstance(lista_de_arquivos, list)
+
+def test_verifica_stime():
+    lista_de_arquivos = retorna_path_de_arquivos(path_raiz)
+
+    calcula_stime(lista_de_arquivos)
+    ...
     
